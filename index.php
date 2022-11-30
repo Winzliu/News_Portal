cb1162c6d90952dbaff4133a55c7a913511d3084
Winzliu <alwin.liufandy@gmail.com>
2022-11-30 22:49:55 +0700

    menambahkan sweetalert pada registrasi

--- index.php
+++ index.php
@@ -1,4 +1,25 @@
 <?php
-echo "selamat datang";
+session_start();
+if (!isset($_SESSION["login"])) {
+  header("Location: login");
+}
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <h1>Selamat Datang</h1>
+  <br>
+  <a href="logout.php">LogOut</a>
+</body>
+
+</html>
